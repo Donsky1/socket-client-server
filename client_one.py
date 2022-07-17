f6019b8576510965a8365fd812310f62d2ee7753
Donsky1 <falson2007@yandex.ru>
2022-07-17 18:44:26 +0300

    update client send function

--- client_one.py
+++ client_one.py
@@ -4,15 +4,8 @@ from client import Client
 
 client1 = Client('Pasha', '', 50004)
 
-
-def send():
-    while True:
-        message = input('Сообщение для отправки: ')
-        client1.send_message(message, 'localhost', 50003)
-
-
 p1 = Thread(target=client1.get_message)
-p2 = Thread(target=send)
+p2 = Thread(target=client1.send_message, args=('localhost', 50003))
 
 p1.start()
 p2.start()
--- client_two.py
+++ client_two.py
@@ -5,14 +5,8 @@ from client import Client
 client2 = Client('Dasha', '', 50003)
 
 
-def send():
-    while True:
-        message = input('Сообщение для отправки: ')
-        client2.send_message(message, 'localhost', 50004)
-
-
 p1 = Thread(target=client2.get_message)
-p2 = Thread(target=send)
+p2 = Thread(target=client2.send_message, args=('localhost', 50004))
 
 p1.start()
 p2.start()
